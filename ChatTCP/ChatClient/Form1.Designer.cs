12d7660575984d129fa06c51f6d0684178e4ffae
Forestf <michalsliwa96@wp.pl>
2019-04-17 20:21:40 +0200

    Server listning for connection

--- ChatTCP/ChatClient/Form1.Designer.cs
+++ ChatTCP/ChatClient/Form1.Designer.cs
@@ -28,59 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelIP = new System.Windows.Forms.Label();
             this.labelPort = new System.Windows.Forms.Label();
-            this.textBoxIP = new System.Windows.Forms.TextBox();
             this.textBoxPort = new System.Windows.Forms.TextBox();
             this.buttonStart = new System.Windows.Forms.Button();
             this.labelStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // labelIP
-            // 
-            this.labelIP.AutoSize = true;
-            this.labelIP.Location = new System.Drawing.Point(13, 13);
-            this.labelIP.Name = "labelIP";
-            this.labelIP.Size = new System.Drawing.Size(23, 13);
-            this.labelIP.TabIndex = 0;
-            this.labelIP.Text = "IP :";
-            // 
             // labelPort
             // 
             this.labelPort.AutoSize = true;
-            this.labelPort.Location = new System.Drawing.Point(13, 40);
+            this.labelPort.Location = new System.Drawing.Point(13, 9);
             this.labelPort.Name = "labelPort";
             this.labelPort.Size = new System.Drawing.Size(32, 13);
             this.labelPort.TabIndex = 1;
             this.labelPort.Text = "Port :";
             // 
-            // textBoxIP
-            // 
-            this.textBoxIP.Location = new System.Drawing.Point(83, 13);
-            this.textBoxIP.Name = "textBoxIP";
-            this.textBoxIP.Size = new System.Drawing.Size(174, 20);
-            this.textBoxIP.TabIndex = 2;
-            // 
             // textBoxPort
             // 
-            this.textBoxPort.Location = new System.Drawing.Point(83, 40);
+            this.textBoxPort.Location = new System.Drawing.Point(182, 9);
             this.textBoxPort.Name = "textBoxPort";
-            this.textBoxPort.Size = new System.Drawing.Size(174, 20);
+            this.textBoxPort.Size = new System.Drawing.Size(75, 20);
             this.textBoxPort.TabIndex = 3;
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(182, 115);
+            this.buttonStart.Location = new System.Drawing.Point(182, 54);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(75, 23);
             this.buttonStart.TabIndex = 4;
             this.buttonStart.Text = "Start Server";
             this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // labelStatus
             // 
             this.labelStatus.AutoSize = true;
-            this.labelStatus.Location = new System.Drawing.Point(13, 115);
+            this.labelStatus.Location = new System.Drawing.Point(13, 54);
             this.labelStatus.Name = "labelStatus";
             this.labelStatus.Size = new System.Drawing.Size(79, 13);
             this.labelStatus.TabIndex = 5;
@@ -90,13 +73,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(269, 150);
+            this.ClientSize = new System.Drawing.Size(269, 93);
             this.Controls.Add(this.labelStatus);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.textBoxPort);
-            this.Controls.Add(this.textBoxIP);
             this.Controls.Add(this.labelPort);
-            this.Controls.Add(this.labelIP);
             this.Name = "Form1";
             this.Text = "Server";
             this.ResumeLayout(false);
@@ -105,10 +86,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label labelIP;
         private System.Windows.Forms.Label labelPort;
-        private System.Windows.Forms.TextBox textBoxIP;
         private System.Windows.Forms.TextBox textBoxPort;
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Label labelStatus;
--- ChatTCP/ChatClient/Form1.cs
+++ ChatTCP/ChatClient/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,39 @@ namespace ChatClient
 {
     public partial class Form1 : Form
     {
+        private static Socket ServerSocket;
+        private static List<TcpClient> Clients;
+        private static byte[] buffer = new byte[1024];
         public Form1()
         {
             InitializeComponent();
+            ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Clients = new List<TcpClient>();
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            int port = 0;
+            try
+            {
+                port = Convert.ToInt32(textBoxPort.Text);
+                if(port <0 || port > 65535)
+                {
+                    MessageBox.Show("Port should be in range beetwen 0 and 65535");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Enter the proper port");
+                return;
+            }
+            StartServer();
+        }
+        private static void StartServer()
+        {
+
         }
     }
 }
