1a4ab6e4f0625a30994dd665702aa14be19181d5
Forestf <michalsliwa96@wp.pl>
2019-04-17 16:22:09 +0200

    Server GUI

--- ChatTCP/ChatClient/Form1.Designer.cs
+++ ChatTCP/ChatClient/Form1.Designer.cs
@@ -28,20 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelIP = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelIP
+            // 
+            this.labelIP.AutoSize = true;
+            this.labelIP.Location = new System.Drawing.Point(13, 13);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(23, 13);
+            this.labelIP.TabIndex = 0;
+            this.labelIP.Text = "IP :";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(13, 40);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(32, 13);
+            this.labelPort.TabIndex = 1;
+            this.labelPort.Text = "Port :";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(83, 13);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(174, 20);
+            this.textBoxIP.TabIndex = 2;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(83, 40);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(174, 20);
+            this.textBoxPort.TabIndex = 3;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(182, 115);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start Server";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(13, 115);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(79, 13);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Server is offline";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(268, 137);
+            this.ClientSize = new System.Drawing.Size(269, 150);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.labelIP);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Server";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
