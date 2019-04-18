aba04597c6bc70aaf52a9e825524412b84fb5d9e
Forestf <michalsliwa96@wp.pl>
2019-04-18 13:35:26 +0200

    Chat GUI

--- ChatTCP/ChatServer/Form1.cs
+++ ChatTCP/ChatServer/Form1.cs
@@ -12,9 +12,46 @@ namespace ChatClient
 {
     public partial class Form1 : Form
     {
+        public string userName;
         public Form1()
         {
             InitializeComponent();
+
+            userName = radioButtonAn.Text;
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                userName = textBoxName.Text;
+            }
+        }
+
+        private void textBoxName_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                //userName = "Anonymous";
+                radioButtonAn.Checked=true;
+            }
+        }
+
+        private void radioButtonNa_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonNa.Checked == true)
+            {
+                textBoxName.Enabled = true;
+            }
+            else
+            {
+                userName = "Anonymous";
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            richTextBoxChat.Text = userName;
         }
     }
 }
