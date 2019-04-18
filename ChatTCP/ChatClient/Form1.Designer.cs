f5a8986d85251998600325954792ded13a982486
Forestf <michalsliwa96@wp.pl>
2019-04-18 12:21:23 +0200

    Server await connections

--- ChatTCP/ChatClient/Form1.Designer.cs
+++ ChatTCP/ChatClient/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ChatClient
+﻿namespace ChatServer
 {
     partial class Form1
     {
@@ -32,6 +32,7 @@
             this.textBoxPort = new System.Windows.Forms.TextBox();
             this.buttonStart = new System.Windows.Forms.Button();
             this.labelStatus = new System.Windows.Forms.Label();
+            this.backgroundWorkerStatus = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // labelPort
@@ -45,14 +46,15 @@
             // 
             // textBoxPort
             // 
-            this.textBoxPort.Location = new System.Drawing.Point(182, 9);
+            this.textBoxPort.Location = new System.Drawing.Point(253, 6);
             this.textBoxPort.Name = "textBoxPort";
             this.textBoxPort.Size = new System.Drawing.Size(75, 20);
             this.textBoxPort.TabIndex = 3;
+            this.textBoxPort.Text = "20333";
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(182, 54);
+            this.buttonStart.Location = new System.Drawing.Point(253, 54);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(75, 23);
             this.buttonStart.TabIndex = 4;
@@ -69,11 +71,15 @@
             this.labelStatus.TabIndex = 5;
             this.labelStatus.Text = "Server is offline";
             // 
+            // backgroundWorkerStatus
+            // 
+            this.backgroundWorkerStatus.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerStatus_RunWorkerCompleted);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(269, 93);
+            this.ClientSize = new System.Drawing.Size(340, 94);
             this.Controls.Add(this.labelStatus);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.textBoxPort);
@@ -90,6 +96,8 @@
         private System.Windows.Forms.TextBox textBoxPort;
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Label labelStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerUpdatelabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerStatus;
     }
 }
 
